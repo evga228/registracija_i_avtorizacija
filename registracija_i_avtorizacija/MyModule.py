8dced903c88c892b5b1d42aa677e3efa2ad6ef07
Evga228 <asus@LAPTOP-3FSUMRHD>
2023-01-16 16:10:16 +0200

    Добавьте файлы проекта.

---
+++ registracija_i_avtorizacija/MyModule.py
@@ -0,0 +1,57 @@
+﻿users = {}
+
+def register():
+    """Rgister
+    :param any username: type username
+    :param any password: tyme password
+    """
+    username = input("Enter a username: ")
+    while True:
+        choice = input("Do you want to generate a password automatically or enter it yourself? (auto/manual) ")
+        if choice.lower() == "auto":
+            import random
+            import string
+            password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+            print("Your password is: " + password)
+            break
+        elif choice.lower() == "manual":
+            password = input("Enter a password: ")
+            validate_password(password)
+            if validate_password(password):
+                print("Password is valid")
+            else:
+                print("Password is invalid")
+            break
+        else:
+            print("Invalid choice.")
+    users[username] = password
+    print("Registration successful!")
+
+def login()->any:
+    """Login
+    :param any username: type username
+    :param any password: tyme password
+    """
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username in users and users[username] == password:
+        print("Login successful!")
+    else:
+        print("Incorrect username or password.")
+
+def validate_password(password:any)->bool:
+    """ Password check
+    :param any password: type password
+    :rtype: bool
+    """
+    if len(password) < 8:
+        return False
+    if not ("[a-z]", password):
+        return False
+    if not ("[A-Z]", password):
+        return False
+    if not ("[0-9]", password):
+        return False
+    if not ("[!@#\$%^&*()]", password):
+        return False
+    return True
---
+++ registracija_i_avtorizacija/registracija_i_avtorizacija.py
@@ -0,0 +1,12 @@
+﻿from MyModule import * 
+
+while True:
+    choice = input("Do you want to register, login or exit? (register/login/exit) ")
+    if choice.lower() == "register":
+        register()
+    elif choice.lower() == "login":
+        login()
+    elif choice.lower() == "exit":
+        break
+    else:
+        print("Invalid choice.")
